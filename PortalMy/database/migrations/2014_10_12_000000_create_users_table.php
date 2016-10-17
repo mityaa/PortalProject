380877616b7efc289d7bfab6a7d436e36e6372ae
mitya <m.nenilin@avia-centr.ru>
2016-10-17 15:03:44 +0300

    Созданиет нового проекта
    Был создан новый проект наплатформе Ларавель, созданы две модели и две миграции к ним.

---
+++ PortalMy/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('second_name');
+            $table->string('third_name');
+            $table->integer('id_users_type')->unsigned();
+            $table->foreign('id_users_type')->references('id')->on('users')->onDelete('cascade');
+            $table->string('password');
+            $table->integer('rate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
